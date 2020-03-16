a876d38869a482fb30c9931e438d852cfe079766
PatrykZarazinski <62218548+PatrykZarazinski@users.noreply.github.com>
2020-03-16 22:02:41 +0100

    Update client.h

--- client/client/include/client.h
+++ client/client/include/client.h
@@ -1,34 +1,36 @@
 #pragma once
 
-#include <arpa/inet.h> // inet_pton()
-#include <cerrno>      //errno
-#include <cstdio>      //perror()
-#include <cstdio>
-#include <cstring> // memset()
-#include <cstring>
-#include <iostream>
 #include <netinet/in.h> //sockaddr_in()
-#include <pthread.h>
-#include <string>
+#include <arpa/inet.h> // inet_pton()
+#include<cstring> // memset()
+#include<iostream>
+#include <sys/types.h> //socket(), bind()
 #include <sys/socket.h> //socket(), bind()
-#include <sys/types.h>  //socket(), bind()
+#include<cstdio> //perror()
+#include<cerrno> //errno
+#include<cstdio>
+#include<cstring>
 #include <unistd.h>
+#include<string>
+#include <pthread.h>
 
-class client {
-private:
-  int mySocket;                          // deskryptor gniazda
-  struct sockaddr_in destinationAddress; // parametry gniazda
-  bool flag;
-  enum { p_size = 512 };
+class client
+{
+    private:
 
-public:
-  client(int serwer_port);
-  ~client();
+    int mySocket; //deskryptor gniazda
+    struct sockaddr_in destinationAddress; //parametry gniazda
+    bool flag;
+    enum{p_size = 512};
 
-  int &get_mySocket() { return mySocket; }
-  struct sockaddr_in *get_destinationAddress() {
-    return &destinationAddress;
-  }
-  void sending();
-  void receive();
+    public:
+
+    client(int serwer_port);
+    ~client();
+
+    int & get_mySocket(){ return mySocket; }
+    struct sockaddr_in * get_destinationAddress(){ return &destinationAddress; }
+    void sending();
+    void receive();
+
 };
